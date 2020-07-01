77b9aa2da00ce617578120323ea5397ef83f4086
vidanio <info@vidanio.com>
2020-07-01 10:06:27 +0200

    fifth attempt

--- main.go
+++ main.go
@@ -21,18 +21,9 @@ func main() {
 		Addr:    ":443",
 		Handler: mux,
 		TLSConfig: &tls.Config{
-			GetCertificate:           certManager.GetCertificate,
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
+			GetCertificate: certManager.GetCertificate,
+			MinVersion:     tls.VersionTLS12,
 		},
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 	}
 
 	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
