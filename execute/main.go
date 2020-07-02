ce5c371a0107c50effd9c721bd04306480c1d92f
vidanio <info@vidanio.com>
2020-07-02 16:56:07 +0200

    playing with exec+3

--- execute/main.go
+++ execute/main.go
@@ -14,7 +14,7 @@ var (
 )
 
 // Lets play with cmdline in linux
-// ffmpeg -re -i url -c copy -flush_packets 0 -f mpegts "udp://127.0.0.1:1234?pkt_size=1316&buffer_size=65535"
+// ffmpeg -re -i url -c copy -flush_packets 0 -f mpegts 'udp://127.0.0.1:1234?pkt_size=1316&buffer_size=65535'
 func main() {
 	comando := "ffmpeg,-re,-i,url,-c,copy,-flush_packets,0,-f,mpegts,-mpegts_original_network_id,1,-mpegts_transport_stream_id,1,-mpegts_service_id,1,-mpegts_pmt_start_pid,129,-mpegts_start_pid,1024,-metadata,service_provider='TodoStreaming',-metadata,service_name='Channel 1',udp://127.0.0.1:1234?pkt_size=1316&buffer_size=65535"
 	cmd := exec.Command("ffmpeg")
@@ -30,21 +30,25 @@ func main() {
 
 	done := make(chan error)
 	cmd.Start()
+	// timer := time.NewTimer(time.Hour * 900000) // beyond a century
+	timer := time.NewTimer(time.Second * 20) // timeout in seconds
 	go func() {
 		done <- cmd.Wait()
 	}()
 
 	// waiting to be killed
 	select {
-	//case <-time.After(time.Hour * 900000): // beyond a century
-	case <-time.After(time.Second * 20): // timeout in seconds
+	case <-timer.C: // timeout in seconds
 		log.Println("Timeout hit...")
 		cmd.Process.Kill()
-		cmd.Wait()
+		<-done // drain the channel
 		return
 	case err := <-done:
 		if err != nil {
 			log.Println("ffmpeg failed:", err)
+		}
+		if !timer.Stop() { // stops timer from firing
+			<-timer.C // drain the channel
 		}
 	}
 
