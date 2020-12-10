bf18a7dec512652aaa0b00674dc3adfb1a8117ab
vidanio <info@vidanio.com>
2020-12-10 09:19:51 +0100

    new autoserver

---
+++ autoserver/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+// This is in our opinion the best config for a server to be compatible, modern and secure at the same time
+// scoring A+ in SSL Labs, IPv6 and HTTP2 with very quick response (have a nice time !!!)
+
+const (
+	globalroot   = "/var/www/html"
+	firstpage    = "index"
+	notfoundfile = "404.html" // inside its own domain's folder
+)
+
+func main() {
+	// setup a simple handler which sends a HTHS header for six months (!)
+	http.HandleFunc("/", root)
+
+	// look for the domains to be served from command line args
+	flag.Parse()
+	domains := flag.Args()
+	if len(domains) == 0 {
+		log.Fatalf("fatal; specify domains as arguments")
+	}
+
+	// create the autocert.Manager with domains and path to the cache
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(domains...),
+	}
+
+	// optionally use a cache dir
+	dir := cacheDir()
+	if dir != "" {
+		certManager.Cache = autocert.DirCache(dir)
+	}
+
+	// create the server itself
+	server := &http.Server{
+		Addr: ":https",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+			MinVersion:     tls.VersionTLS12,
+		},
+	}
+
+	log.Printf("Serving http/https for domains: %+v", domains)
+	go func() {
+		// serve HTTP, which will redirect automatically to HTTPS
+		h := certManager.HTTPHandler(nil)
+		log.Fatal(http.ListenAndServe(":http", h))
+	}()
+
+	// serve HTTPS!
+	log.Fatal(server.ListenAndServeTLS("", ""))
+}
+
+// cacheDir makes a consistent cache directory inside /tmp. Returns "" on error.
+func cacheDir() (dir string) {
+	if u, _ := user.Current(); u != nil {
+		dir = filepath.Join(os.TempDir(), "cache-golang-autocert-"+u.Username)
+		if err := os.MkdirAll(dir, 0700); err == nil {
+			return dir
+		}
+	}
+	return ""
+}
+
+// web server for root
+func root(w http.ResponseWriter, req *http.Request) {
+	var out string
+	var notfound bool
+
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+
+	out = fmt.Sprintf("Host: %s\n", req.Host)
+	for k, v := range req.Header {
+		out += fmt.Sprintf("%s : %s\n", k, v[0])
+	}
+	fmt.Printf(out)
+
+	rootdir := fmt.Sprintf("%s/%s/", globalroot, req.Host) // "/var/www/html/domain.com/"
+	namefile := strings.TrimRight(rootdir+req.URL.Path[1:], "/")
+	fileinfo, err := os.Stat(namefile)
+	if err != nil {
+		// file does not exist
+		namefile = fmt.Sprintf("%s%s", rootdir, notfoundfile)
+		notfound = true
+	} else if fileinfo.IsDir() {
+		// it is a folder, get in and access the index page
+		namefile = namefile + "/" + firstpage + ".html"
+		_, err := os.Stat(namefile)
+		if err != nil {
+			// no index file inside this folder
+			namefile = fmt.Sprintf("%s%s", rootdir, notfoundfile)
+			notfound = true
+		}
+	}
+	fmt.Println("File:", namefile)
+	if notfound {
+		fileinfo, err = os.Stat(namefile)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+	}
+	fr, err := os.Open(namefile)
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	defer fr.Close()
+
+	http.ServeContent(w, req, namefile, fileinfo.ModTime(), fr)
+}
